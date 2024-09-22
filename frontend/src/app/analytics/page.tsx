9a14c3e160b4c1eb44b8473f00fd3c4e1f65bfb6
SuveenE <suveen.te1@gmail.com>
2024-09-21 17:44:41 -0700

    Update pages

---
+++ frontend/src/app/analytics/page.tsx
@@ -0,0 +1,7 @@
+export default function Analytics() {
+  return (
+    <div className="flex flex-col min-h-screen p-12 gap-4">
+      <p className="mr-2 text-xl font-semibold tracking-tight">Look at your stats!</p>
+    </div>
+  );
+}
--- frontend/src/app/recall/page.tsx
+++ frontend/src/app/recall/page.tsx
@@ -2,9 +2,9 @@ import { MicIcon } from "lucide-react";
 
 export default function Recall() {
   return (
-    <div className="flex flex-col min-h-screen mt-36 items-center  p-12 gap-4">
+    <div className="flex flex-col min-h-screen p-12 gap-4">
       <p className="mr-2 text-xl font-semibold tracking-tight">What are you trying to recall?</p>
-      <div className="border rounded-full border-2 border-black w-min p-5"><MicIcon size={80}/></div>
+      <div className="border rounded-full border-2 hover:bg-slate-200 border-black w-min p-5 cursor-pointer hover:scale-105 ease-in-out duration-300"><MicIcon size={60}/></div>
     </div>
   );
 }
